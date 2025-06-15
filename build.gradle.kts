b1a8f3adfc5d99d372fb1f6d0072d3252c8fd876
人間工作 <coolxiaom95@gmail.com>
2025-06-15 10:15:10 +0800

    gradle-ify

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    java
+    `maven-publish`
+}
+
+group = "top.mrxiaom"
+version = "6.7.6"
+val targetJavaVersion = 8
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.20-R0.1-SNAPSHOT")
+    implementation("org.jetbrains:annotations:24.0.0")
+}
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+    }
+}
+tasks {
+    withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+        if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
+            options.release.set(targetJavaVersion)
+        }
+    }
+    processResources {
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        from(sourceSets.main.get().resources.srcDirs) {
+            expand(mapOf("version" to version))
+            include("plugin.yml")
+        }
+    }
+}
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components.getByName("java"))
+            groupId = project.group.toString()
+            artifactId = rootProject.name
+            version = project.version.toString()
+        }
+    }
+}
